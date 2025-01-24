bbe902cf2d07e99bd29c3a1b2da6272c33b8f48b
May Clean Sitepu <113108027+MayCleanSitepu@users.noreply.github.com>
2025-01-24 20:12:53 +0700

    Delete Models directory

--- Models/AddStudentDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Kampus.Models
-{
-    public class AddStudentDto
-    {
-        public required string Name { get; set; }
-        public required string? LastName { get; set; }
-        public required DateTime Bithday { get; set; }
-    }
-}
